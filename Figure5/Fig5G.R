cadd3e443bbeaa9f5c7682b2af9400e83240a09c
seenstevo <seenstevo@protonmail.com>
2020-01-07 16:26:08 +0000

    Create Fig5G.R

---
+++ Figure5/Fig5G.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(gplots)
+library(viridis)
+
+df <- read_tsv("Fig5G_motif_rank_data.txt", col_names = c("Motif","AtC3", "GgC3", "AtC4", "GgC4"))
+
+df_matrix <- as.matrix(df[,-1])
+
+rownames(df_matrix) <- df$Motif
+
+col_breaks <- c(seq(1,50,length=300), seq(51,300,length=300), seq(301,841, length=300))
+
+svg(filename = "Fig5G.svg", height = 10, width = 5, pointsize = 12)
+
+heatmap.2(df_matrix, scale='none', Colv=F, cexRow=0.9, cexCol = 2, 
+          trace = "none", key = TRUE, keysize = 1, key.title = NA, 
+          col=cividis, offsetCol = 0, margins = c(14,9), breaks = col_breaks,
+          lhei = c(1,5), lwid = c(1,2))
+dev.off()
