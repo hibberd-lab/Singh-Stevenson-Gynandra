e707cec09f4d7b89f57e6ec3f968262e7d018ddf
seenstevo <seenstevo@protonmail.com>
2020-06-24 15:54:48 +0100

    Update and rename Fig6G.R to Fig6D.R

--- Figure6/Fig6D.R
+++ Figure6/Fig6D.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(gplots)
 library(viridis)
 
-df <- read_tsv("Fig5G_motif_rank_data.txt", col_names = c("Motif","AtC3", "GgC3", "AtC4", "GgC4"))
+df <- read_tsv("Fig5D_motif_rank_data.txt", col_names = c("Motif","AtC3", "GgC3", "AtC4", "GgC4"))
 
 df_matrix <- as.matrix(df[,-1])
 
@@ -10,7 +10,7 @@ rownames(df_matrix) <- df$Motif
 
 col_breaks <- c(seq(1,50,length=300), seq(51,300,length=300), seq(301,841, length=300))
 
-svg(filename = "Fig5G.svg", height = 10, width = 5, pointsize = 12)
+svg(filename = "Fig5D.svg", height = 10, width = 5, pointsize = 12)
 
 heatmap.2(df_matrix, scale='none', Colv=F, cexRow=0.9, cexCol = 2, 
           trace = "none", key = TRUE, keysize = 1, key.title = NA, 
