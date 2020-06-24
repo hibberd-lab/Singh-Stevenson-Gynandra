60018e2a0eaf816324a9342592e750204b01c91e
seenstevo <seenstevo@protonmail.com>
2020-06-24 16:00:27 +0100

    Update and rename Heatmap.R to Fig5B.R

--- Figure6/Fig5B.R
+++ Figure6/Fig5B.R
@@ -2,14 +2,14 @@ library(tidyverse)
 library(gplots)
 library(viridis)
 
-df <- read_tsv("Fig5A_heatmap_data.txt", col_names = F)
+df <- read_tsv("Fig5B_heatmap_data.txt", col_names = F)
 
 df_matrix <- as.matrix(df[,2:5])
 rownames(df_matrix) <- df$X1
 
 col_breaks <- c(seq(-1,-0.1,length=100), seq(-0.09,0.1,length=100), seq(0.11,0.5, length=100))
 
-svg(filename = "Fig5C.svg", height = 10, width = 5, pointsize = 20)
+svg(filename = "Fig5B.svg", height = 10, width = 5, pointsize = 20)
 
 heatmap.2(df_matrix, Colv = F, col = viridis, RowSideColors = 
             c(rep("grey", 87), rep("black", 23)), dendrogram = "row", 
