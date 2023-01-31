14b9bfc922645523667b2bd9d082e990380e2b6c
seenstevo <seenstevo@protonmail.com>
2023-01-31 21:39:01 +0200

    Update Fig3E.R

--- Figure3/Fig3E.R
+++ Figure3/Fig3E.R
@@ -1,8 +1,6 @@
 library(tidyverse)
 library(ggplot2)
 
-setwd("/data/Data_Storage/new_MAY_DNase/Feb21_Overlap_Check/New_HyperConserved")
-
 theme_set(theme_bw())
 theme_update(axis.text.x = element_text(angle = 45, hjust = 1))
 
@@ -14,9 +12,7 @@ clusters_filtered <- clusters_filtered[["Cluster"]]
 
 data <- data[data$Cluster %in% as.vector(clusters_filtered),]
 
-svg("Light_Only_DHS_Enriched_Motifs.svg", width = 4, height = 4)
 ggplot(data, aes(x = reorder(factor(Cluster),-`log(Obs/Exp)`, FUN=median, desc =TRUE), y = `log(Obs/Exp)`, group = Cluster))+
   geom_boxplot()+
   geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.5)+
   labs(x = "Motif Clusters", y = "Light Specific DHS Motif Enrichment (log ratio)")
-dev.off()
